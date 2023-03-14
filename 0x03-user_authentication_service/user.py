1413459ad6e037121a6265539f42a21dd1fcd198
doloadama <doloadama@outlook.com>
2023-03-14 16:22:30 +0000

    pycodestyle

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -18,9 +18,10 @@ class User(Base):
 
     id = Column(Integer, primary_key=True)
     email = Column(String(250), nullable=False)
-    hashed_password = Column(String(250), nullable=True)
+    hashed_password = Column(String(250), nullable=False)
     session_id = Column(String(250), nullable=True)
     reset_token = Column(String(250), nullable=True)
 
     def __repr__(self) -> str:
         return f"<User(id={self.id}, email={self.email}, hashed_password={self.hashed_password})>"
+    
