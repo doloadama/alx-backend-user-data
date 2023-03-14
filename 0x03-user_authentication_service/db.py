d77bb81e719592ccf4a275fd3b8ef85db6b11ea8
doloadama <doloadama@outlook.com>
2023-03-14 16:10:54 +0000

    fixed

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -35,9 +35,12 @@ class DB:
         """
         Add a new user to the database.
         """
+        if not email or not hashed_password:
+            return
         user = User(email=email, hashed_password=hashed_password)
-        self._session.add(user)
-        self._session.commit()
+        session = self._session
+        session.add(user)
+        session.commit()
         return user
 
     def find_user_by(self, **kwargs) -> User:
